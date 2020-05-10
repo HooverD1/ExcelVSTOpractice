1033cf336a5681d34583c50417b0fa1a58d33c4e
dhoover <davehoover1985@gmail.com>
2020-05-10 13:00:20 -0500

    Added additional stat tests

--- NUnit_Tests/UnitTest1.cs
+++ NUnit_Tests/UnitTest1.cs
@@ -15,18 +15,32 @@ namespace Tests
         }
 
         [Test]
-        public void Test_LognormalEstimate_score()
+        public void Test_Score_LognormalEstimate()
         {
             var logNorm = new LognormalEstimate(test_data);
             logNorm.Construct();
             Assert.AreEqual(logNorm.score, 0.97989354960026254);
         }
         [Test]
-        public void Test_NormalEstimate()
+        public void Test_Score_NormalEstimate()
         {
             var norm = new NormalEstimate(test_data);
             norm.Construct();
             Assert.AreEqual(norm.score, 0.99975274853711238);
+        }
+        [Test]
+        public void Test_Score_ExponentialEstimate()
+        {
+            var expo = new ExponentialEstimate(test_data);
+            expo.Construct();
+            Assert.AreEqual(expo.score, 6.118046410036521E-07);
+        }
+        [Test]
+        public void Test_Score_UniformEstimate()
+        {
+            var uniform = new UniformEstimate(test_data);
+            uniform.Construct();
+            Assert.AreEqual(uniform.score, 0.0);
         }
     }
 }
